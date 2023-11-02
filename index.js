134b25fed6f2396ee2bc7d65b6326f87023d259f
Iryn Abigail Delim <ipd5759@nyu.edu>
2023-11-02 15:24:05 -0400

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const indexRoute = require("./routes/index");
+const aboutRoute = require("./routes/about");
+
+app.use("/", indexRoute);
+app.use("/about", aboutRoute);
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.send("Hellooo");
+})
+
+app.listen(port, ()=> {
+    console.log(`Example app listening on port ${port}`)
+})
